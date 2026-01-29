e118af6d8232bb7d4e830def479868ab24ac1350
Edgar Carreño Fonseca <edgarcarrenofonseca@outlook.com>
2026-01-29 17:59:36 -0600

    First commit on the repo - def configs.

---
+++ app/src/main/java/com/example/simplecomposecicdandroidproject/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.simplecomposecicdandroidproject
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.simplecomposecicdandroidproject.ui.theme.SimpleComposeCICDAndroidProjectTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SimpleComposeCICDAndroidProjectTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    // Implement compose navigator and generic classes for view model and MVI managers
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SimpleComposeCICDAndroidProjectTheme {
+        Greeting("Android")
+    }
+}
